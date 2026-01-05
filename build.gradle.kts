ffa0fb51dd027834c830f16d9adc8009d7f6c612
Park Jaehyun <earlydreamer@users.noreply.github.com>
2026-01-05 15:37:10 +0900

    0008:의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     runtimeOnly("org.springframework.boot:spring-boot-docker-compose")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
 }
 
 tasks.withType<Test> {
